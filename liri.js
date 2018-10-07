5c392fb691b1fb5b3337d4b96371ce0d0c8f238f
Perri Specter <plspecter@gmail.com>
2018-10-07 17:04:41 -0400

    liribot.js

--- liri.js
+++ liri.js
@@ -1,15 +1,38 @@
-//request npm first
-var request = require("request");
+//get the apis requires
+var omdbpai = require("omdpai");
+var napster = require("napster");
 
+//call the api here?
+var options = {
+    provider: "omdbpai",
+    apikey: "tt3896198&apikey=cd19a446",  
+};
+
+var omdbapi = omdbapiOptions(options);
 //the movie or song you type in the terminal will be the third argument
-var Moviename = process.agrv[2];
 var Songname = process.agrv[2];
+
+
+
 //this is the movie api (omdbapi)
 
+//or here?
 var queryUrl = "http://www.omdbapi.com/?t=" + Moviename + "tt3896198&apikey=cd19a446";
 console.log(queryUrl);
 
+var Moviename = process.agrv[2];
 
+//
+request(queryUrl, function(error, response, body) {
+//if the request works then..
+
+if (!error && response.statusCode === 200) {
+
+    console.log ()
+
+}
+
+})
 
 
 //this is the music api
