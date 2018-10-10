6d170df2ae3f676e3a4588ce4a4e2ab5e21977d9
Perri Specter <plspecter@gmail.com>
2018-10-10 14:13:57 -0400

    liri.js

--- liri.js
+++ liri.js
@@ -1,8 +1,9 @@
 
 require("dotenv").config();
+require("bandsintown").config();
 
 var keys=require("./keys.js")//we are importing keys file here using request method
-var spotify = new spotify(keys.spotify);//access spotify keys
+// var spotify = new spotify(keys.spotify);//access spotify keys
 
 //var spotify = new Spotify(keys.spotify);//this will give acess to spotify object in keys.js
 
@@ -18,18 +19,24 @@ var movieName = process.argv[2];
 var artist = process.argv[2];
 
 
+//Create an empty variable for holding the song name
+var song = process.argv[2];
+
+
+
 // Then run a request to the OMDB API with the movie specified
 var omdbapi = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 //for bandsintown api
 var bandsintown = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+//not sure if this is the right html link?
+var spotify = "https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums?album_type=SINGLE&offset=20&limit=10" + song + "4304517fc1f640a99a614b5b73c4e64a"
 
-var spotify = "" + song + "4304517fc1f640a99a614b5b73c4e64a"
 
 
-
-// This line is just to help us debug against the actual URL.
+//Console log the apis'
 console.log(omdbapi);
 console.log(bandsintown);
+console.log(spotify);
 
 request(omdbapi, function(error, response, body) {
 
@@ -61,7 +68,7 @@ request(spotify, function(error, response, body) {
   console.log(body)
       // Parse the body of the site and recover just the imdbRating
       // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-      console.log(" " + JSON.parse(body).Year);
+      console.log("This song is " + JSON.parse(body).Year);
     }
   });
 
